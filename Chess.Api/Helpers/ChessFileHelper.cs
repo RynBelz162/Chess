3c115277b3b406f40fe6d0d105b0d9792bd7414e
RynBelz162 <rynbelz162@gmail.com>
2022-02-05 18:43:17 -0500

    ✨ implement bishop

--- Chess.Api/Helpers/ChessFileHelper.cs
+++ Chess.Api/Helpers/ChessFileHelper.cs
@@ -4,26 +4,31 @@ namespace Chess.Api.Helpers;
 
 public static class ChessFileHelper 
 {
+    private static readonly ChessFile[] _allFiles = Array.Empty<ChessFile>();
     public static readonly char[] OrderedFiles;
     static ChessFileHelper()
     {
+        if (!_allFiles.Any())
+        {
+            _allFiles = Enum.GetValues<ChessFile>();
+        }
+
         OrderedFiles = GetOrderedFiles();
     }
 
     public static (ChessFile? leftFile, ChessFile? rightFile) GetLeftAndRightFile(ChessFile currentFile)
     {
-        var files = Enum.GetValues<ChessFile>();
-        var currIndex = Array.IndexOf(files, currentFile);
+        var currIndex = Array.IndexOf(_allFiles, currentFile);
 
-        ChessFile? leftFile = currIndex == 0 ? null : files[currIndex - 1];
-        ChessFile? rightFile = (currIndex == files.Length - 1) ? null : files[currIndex + 1];
+        ChessFile? leftFile = currIndex == 0 ? null : _allFiles[currIndex - 1];
+        ChessFile? rightFile = (currIndex == _allFiles.Length - 1) ? null : _allFiles[currIndex + 1];
 
         return (leftFile, rightFile);
     }
 
-        private static char[] GetOrderedFiles()
+    private static char[] GetOrderedFiles()
     {
-        var files = Enum.GetValues<ChessFile>()
+        var files = _allFiles
             .Select(x => ((char)x))
             .ToArray();
 

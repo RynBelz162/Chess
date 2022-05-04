b2a58c38c717a05fb5d67d9e66906f799e4c38c7
RynBelz162 <rynbelz162@gmail.com>
2022-05-03 21:00:32 -0400

    ✨ implement queen and tests

---
+++ Chess.Shared.Tests/Models/Pieces/QueenTests.cs
@@ -0,0 +1,57 @@
+using System.Linq;
+using Chess.Shared.Constants;
+using Chess.Shared.Models.Pieces;
+
+namespace Chess.Shared.Tests.Models.Pieces;
+
+public class QueenTests
+{
+    [Fact]
+    public void RecalculateAvailableMoves_MiddleOfBoard_MovesAllDirections()
+    {
+        var queen = new Queen { Color = ChessColor.White };
+
+        var board = new ChessBoardBuilder()
+            .PlacePieceAt(queen, ChessFile.D, 4)
+            .Build();
+
+        var moves = queen.RecalculateAvailableMoves(board);
+        var expectedMoves = new List<string>
+        {
+            "A4", "B4", "C4", "E4", "F4", "G4", "H4", // left to right
+            "D1", "D2", "D3", "D5", "D6", "D7", "D8", // top to bottom
+            "C5", "B6", "A7", "E5", "F6", "G7", "H8", // top diagonals
+            "C3", "B2", "A1", "E3", "F2", "G1" // bottom diagonals
+        };
+
+        moves.Should().BeEquivalentTo(expectedMoves);
+    }
+
+    [Fact]
+    public void RecalculateAvailableMoves_MiddleOfBoard_CaptureAllDirections()
+    {
+        var queen = new Queen { Color = ChessColor.White };
+        var board = new ChessBoardBuilder()
+            .PlacePieceAt(queen, ChessFile.E, 4)
+            .CreatePieceAt('p', ChessFile.B, 4)
+            .CreatePieceAt('q', ChessFile.E, 7)
+            .CreatePieceAt('r', ChessFile.F, 4)
+            .CreatePieceAt('n', ChessFile.E, 2)
+            .CreatePieceAt('P', ChessFile.C, 6)
+            .CreatePieceAt('p', ChessFile.H, 7)
+            .CreatePieceAt('R', ChessFile.B, 1)
+            .CreatePieceAt('N', ChessFile.F, 3)
+            .Build();
+
+        var moves = queen.RecalculateAvailableMoves(board);
+        var expectedMoves = new List<string>
+        {
+            "B4", "C4", "D4", "F4", // left to right
+            "E5", "E6", "E7", "E3", "E2", // top to bottom
+            "D5", "F5", "G6", "H7", // top diagonals
+            "D3", "C2", // bottom diagonal
+        };
+
+        moves.Should().BeEquivalentTo(expectedMoves);
+    }
+}
--- Chess.Shared/Models/Pieces/Queen.cs
+++ Chess.Shared/Models/Pieces/Queen.cs
@@ -1,10 +1,49 @@
+using Chess.Shared.Constants;
+using Chess.Shared.Extensions;
+
 namespace Chess.Shared.Models.Pieces;
 
 public class Queen : Piece
 {
-    // TODO: implement
     public override List<string> RecalculateAvailableMoves(Board board)
     {
-        throw new NotImplementedException();
+        var moves = new List<string>();
+        Func<ChessFile, int, MovementResult> canMoveToSquare = (file, rank) =>
+        {
+            var targetSquare = $"{file}{rank}";
+            var isOccupied = board.IsSquareOccupied(targetSquare);
+            
+            if (!isOccupied)
+            {
+                return new MovementResult(false, file, rank);
+            }
+
+            if (board.PieceColorOnSqaure(targetSquare) != Color)
+            {
+                return new MovementResult(true, file, rank);
+            }
+
+            return new MovementResult(true, null, null);
+        };
+
+        Action<MovementResult> addToMoves = result =>
+        {
+            if (!result.HasTargetSquare) return;
+            moves.Add($"{result.TargetFile}{result.TargetRank}");
+        };
+
+        // left, right, up, down
+        this.Forward(canMoveToSquare, addToMoves);
+        this.Backward(canMoveToSquare, addToMoves);
+        this.Left(canMoveToSquare, addToMoves);
+        this.Right(canMoveToSquare, addToMoves);
+
+        // diagonals
+        this.DiagonalBottomLeft(canMoveToSquare, addToMoves);
+        this.DiagonalBottomRight(canMoveToSquare, addToMoves);
+        this.DiagonalTopLeft(canMoveToSquare, addToMoves);
+        this.DiagonalTopRight(canMoveToSquare, addToMoves);
+
+        return moves;
     }
 }

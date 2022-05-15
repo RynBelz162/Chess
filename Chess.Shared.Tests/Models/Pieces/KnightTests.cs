4d5f470147f26170610584a33d344f055e4d39ed
RynBelz162 <rynbelz162@gmail.com>
2022-05-14 21:30:01 -0400

    ✅ add more tests

--- Chess.Shared.Tests/Models/Pieces/KnightTests.cs
+++ Chess.Shared.Tests/Models/Pieces/KnightTests.cs
@@ -23,5 +23,45 @@ public class KnightTests
         availableMoves.Should().BeEquivalentTo(expectedMoves);
     }
 
-    // TODO: Finish adding more tests
+    [Fact]
+    public void RecaclulateAvailableMoves_EdgeOfBoard_CantMoveLeft()
+    {
+        var knight = new Knight { Color = ChessColor.White };
+
+        var board = new ChessBoardBuilder()
+            .PlacePieceAt(knight, ChessFile.A, 3)
+            .CreatePieceAt('P', ChessFile.C, 4)
+            .CreatePieceAt('k', ChessFile.B, 5)
+            .Build();
+
+        var availableMoves = knight.RecalculateAvailableMoves(board);
+
+        var expectedMoves = new List<string>
+        {
+            "B5", "C2", "B1"
+        };
+
+        availableMoves.Should().BeEquivalentTo(expectedMoves);
+    }
+
+    [Fact]
+    public void RecaclulateAvailableMoves_EdgeOfBoard_CantMoveRight()
+    {
+        var knight = new Knight { Color = ChessColor.White };
+
+        var board = new ChessBoardBuilder()
+            .PlacePieceAt(knight, ChessFile.H, 3)
+            .CreatePieceAt('p', ChessFile.F, 4)
+            .CreatePieceAt('k', ChessFile.G, 5)
+            .Build();
+
+        var availableMoves = knight.RecalculateAvailableMoves(board);
+
+        var expectedMoves = new List<string>
+        {
+            "G5", "F4", "F2", "G1"
+        };
+
+        availableMoves.Should().BeEquivalentTo(expectedMoves);
+    }
 }
